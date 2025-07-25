c1d3fe328859e298a865820a0c52318b9be71538
kirillkruglikov <kirill.k@xperience.lv>
2025-07-25 17:35:17 +0300

    created funcs for parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,15 @@
+use crate::Config;
+use std::fs;
+
 struct Dataset {
     headers: Vec<String>,
     rows: Vec<Vec<String>>,
     row_count: Option<usize>,
     file_size: Option<usize>,
+}
+
+pub fn load_dataset(config: &Config) {
+    let content = fs::read_to_string(config.filename()).unwrap();
+    println!("Successfuly loaded csv from {}", config.filename());
+    println!("{content}");
 }
