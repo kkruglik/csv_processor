cae41f9d6e33f67552c3d44f6ea30cc2a4dbf072
kirillkruglikov <kirillkruglikov@gmail.com>
2025-07-26 17:05:47 +0300

    Implementer parsing dataset with csv

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,86 @@
 use crate::Config;
+use csv::Reader;
 use std::fs;
 
-struct Dataset {
-    headers: Vec<String>,
-    rows: Vec<Vec<String>>,
-    row_count: Option<usize>,
-    file_size: Option<usize>,
+#[derive(Debug)]
+pub enum CsvError {
+    IoError(std::io::Error),
+    ParseError(csv::Error),
+    FileNotFound(String),
 }
 
-pub fn load_dataset(config: &Config) {
-    let content = fs::read_to_string(config.filename()).unwrap();
+#[derive(Debug)]
+pub struct Dataset {
+    headers: Vec<String>,
+    rows: Vec<Vec<String>>,
+    file_size: usize,
+    rows_count: usize,
+    cols_count: usize,
+}
+
+impl Dataset {
+    fn new(headers: Vec<String>, rows: Vec<Vec<String>>, file_size: usize) -> Dataset {
+        let rows_count = rows.len();
+        let cols_count = headers.len();
+
+        Dataset {
+            headers,
+            rows,
+            file_size,
+            rows_count,
+            cols_count,
+        }
+    }
+
+    pub fn headers(&self) -> &[String] {
+        &self.headers
+    }
+
+    pub fn rows(&self) -> &[Vec<String>] {
+        &self.rows
+    }
+
+    pub fn file_size(&self) -> usize {
+        self.file_size
+    }
+
+    pub fn row_count(&self) -> usize {
+        self.rows_count
+    }
+
+    pub fn col_count(&self) -> usize {
+        self.cols_count
+    }
+}
+
+impl From<std::io::Error> for CsvError {
+    fn from(error: std::io::Error) -> Self {
+        CsvError::IoError(error)
+    }
+}
+
+impl From<csv::Error> for CsvError {
+    fn from(error: csv::Error) -> Self {
+        CsvError::ParseError(error)
+    }
+}
+
+pub fn load_dataset(config: &Config) -> Result<Dataset, CsvError> {
+    let mut reader = Reader::from_path(config.filename())?;
+    let file_metadata = fs::metadata(config.filename())?;
+    let file_size = file_metadata.len() as usize;
+
     println!("Successfuly loaded csv from {}", config.filename());
-    println!("{content}");
-    println!("Exiting")
+
+    let headers: Vec<String> = reader.headers()?.iter().map(|h| h.to_string()).collect();
+
+    let mut rows = Vec::new();
+
+    for result in reader.records() {
+        let record = result?;
+        let row = record.iter().map(|r| r.to_string()).collect();
+        rows.push(row);
+    }
+
+    Ok(Dataset::new(headers, rows, file_size))
 }
